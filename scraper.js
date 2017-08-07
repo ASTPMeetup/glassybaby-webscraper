dd7bc6c00abbaf6f5af429f8df8acbef9d9e5583
ASTPMeetup <pretto417@gmail.com>
2017-08-06 22:42:38 -0500

    add csv output

--- scraper.js
+++ scraper.js
@@ -1,8 +1,8 @@
 'use strict';
 const scrapeIt = require("scrape-it");
 const request = require('request');
-// const json2csv = require('json2csv');
-// const fs = require('fs');
+const json2csv = require('json2csv');
+const fs = require('fs');
 const productArray = [];
 const openWSDatabase = 'https://openws.herokuapp.com/glassybaby?apiKey=8fa0e46f0361117d65d91d6032391324';
 let scrapedData = [];
@@ -326,21 +326,18 @@ function runScraper(data) {
         }, (err, res) => {
             productArray.push(res);
             if (productArray.length == 260) {
-                console.log(productArray);
+                createCSVFile(productArray);
             }
         });
     });
 }
 
+function createCSVFile(productList){
+    var csv = json2csv({ data: productList});
 
-// var csv = json2csv({ data: myCars, fields: fields });
-//
-// fs.writeFile('glassybaby.csv', csv, function(err) {
-//     if (err) throw err;
-//     console.log('file saved');
-
-// runScraper(productData).then(v => {
-//   console.log(v);  // prints 60 after 2 seconds.
-// });
-
+    fs.writeFile('glassybaby.csv', csv, function(err) {
+        if (err) throw err;
+            console.log('file saved');
+    });
+}
 httpGetProducts();
